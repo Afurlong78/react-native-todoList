909a7851987206a3ea6eda1f2f6fbbf90b6e955e
afurlong <afurlong78@gmail.com>
2022-09-30 13:38:35 -0400

    mobile todo list

---
+++ App.js
@@ -0,0 +1,56 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import React, { useState } from "react";
+import Header from "./components/Header";
+import TodoItem from "./components/TodoItem";
+import AddTodo from "./components/AddTodo";
+
+export default function App() {
+  const [todos, setTodos] = useState([
+    { text: "buy coffee", key: 1 },
+    { text: "make app", key: 2 },
+    { text: "apply for a job", key: 3 },
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.key != key);
+    });
+  };
+
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [{ text: text, key: Math.random().toString() }, ...prevTodos];
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.content}>
+        <AddTodo submitHandler={submitHandler} />
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  },
+});
